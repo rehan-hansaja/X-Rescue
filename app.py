aeec09b25c13ad203b22f82617646805756dc5a4
rehan-hansaja <rehan.hansaja@atlinkcom.com>
2026-02-03 13:35:47 +0530

    Added port to host

--- app.py
+++ app.py
@@ -100,4 +100,6 @@ def contact():
     return render_template("contact.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 4000))
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=port)
